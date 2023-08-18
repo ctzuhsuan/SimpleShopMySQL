c5f0afb3e2951c9ef119ad8b047f98a8a65067de
tzuhsuannn <tzuhsuannn@gmail.com>
2023-08-18 13:55:03 +0800

    edit

--- src/main/java/com/example/simpleshopmysql/repo/LineItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.simpleshopmysql.repo;
-
-import com.example.simpleshopmysql.models.LineItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LineItemRepository extends JpaRepository<LineItem, Integer> {
-}
--- src/main/java/com/example/simpleshopmysql/repo/SaleOrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.simpleshopmysql.repo;
-
-import com.example.simpleshopmysql.models.SaleOrder;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SaleOrderRepository extends JpaRepository<SaleOrder,Integer> {
-}
